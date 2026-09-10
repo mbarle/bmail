c57da271218b249a4c1db0d6a82094893ea6ee92
mbarle <marusa.barle@gmail.com>
2026-09-10 11:42:07 -0700

    Removed models.py and app.yaml files.

--- models.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'Fujitsu'
-from google.appengine.ext import ndb
-
-class Sporocilo(ndb.Model):
-    Posiljatelj = ndb.StringProperty()
-    Naslovnik = ndb.StringProperty()
-    Message = ndb.StringProperty()
-    nastanek = ndb.DateTimeProperty(auto_now_add=True)
